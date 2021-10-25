bfd724bc4e064d1ee4a5cda8fd936cca9d3b59ed
ilyasofficial1617 <ilyasofficial1617@gmail.com>
2021-10-26 01:19:23 +0700

    added training 80/20 information

---
+++ GenerateImageListTxt.py
@@ -0,0 +1,22 @@
+import os
+import random
+import shutil
+import sys
+
+def get_files_no_txt_list(dir_location):
+  all_files = os.listdir(dir_location)
+  filenames = []
+  
+  for file in all_files:
+    if file.endswith(".txt"):
+      #skip txt
+      continue
+    else :
+      filenames.append(  file  )
+  return filenames
+
+folder = sys.argv[1]
+images = get_files_no_txt_list(folder)
+with open(folder+'/files.txt', 'w') as f:
+  for image in images:
+    f.write(image+"\n")
